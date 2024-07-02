2d8935674c5b0618abde6dfd132bf096f53c920b
basicallygit <91993321+basicallygit@users.noreply.github.com>
2024-07-02 23:03:08 +0100

    Whoops forgot the prompt argument

--- lib/io.c
+++ lib/io.c
@@ -24,32 +24,35 @@
 // Increment in chunks of 32 bytes per realloc
 #define READLINE_BUF_INCREMENT 32
 
-char* readline(void) {
-	char* input = malloc(READLINE_BUF_INCREMENT);
-	if (input == NULL)
-		return NULL;
+char* readline(const char* prompt) {
+    if (prompt != NULL)
+	printf("%s", prompt);
+		
+    char* input = malloc(READLINE_BUF_INCREMENT);
+    if (input == NULL)
+	return NULL;
 	
-	size_t size = 0;
-	size_t capacity = READLINE_BUF_INCREMENT;
+    size_t size = 0;
+    size_t capacity = READLINE_BUF_INCREMENT;
 
-	int c;
-	while((c = getchar()) != '\n' && c != EOF) {
-		if (size + 1 >= capacity) {
-			capacity += READLINE_BUF_INCREMENT;
-			char* temp = realloc(input, capacity);
+    int c;
+    while((c = getchar()) != '\n' && c != EOF) {
+	if (size + 1 >= capacity) {
+	    capacity += READLINE_BUF_INCREMENT;
+	    char* temp = realloc(input, capacity);
 			
-			if (temp == NULL) {
-				free(input);
-				return NULL;
-			}
-			input = temp;
-		}
-		input[size++] = c;
+	    if (temp == NULL) {
+		free(input);
+		return NULL;
+	    }
+	    input = temp;
 	}
+	input[size++] = c;
+    }
 
-	input[size] = '\0';
+    input[size] = '\0';
 
-	return input;
+    return input;
 }
 
 char* readpassword(const char* prompt) {
